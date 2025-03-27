646b073d2af258ea86ab750a9c177052f91a7334
doraemori <doraemori@users.noreply.github.com>
2025-03-28 06:19:21 +0900

    ビルド

--- build/text-typing.js
+++ build/text-typing.js
@@ -1,44 +1,35 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
-  (global = global || self, global.ShuffleText = factory());
-}(this, function () { 'use strict';
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.TextTyping = factory());
+})(this, (function () { 'use strict';
 
-  /**
-   * ShuffleText is random text effect class for DOM Elements.
-   * ShuffleTextはDOMエレメント用ランダムテキストクラスです。
-   * @author Yasunobu Ikeda
-   * @since 2012-02-07
-   */
-  var ShuffleText = /** @class */ (function () {
+  class TextTyping {
       /**
-       * Constructor.
        * @param element DOMエレメントです。
        */
-      function ShuffleText(element) {
+      constructor(element) {
           /**
            * The string for random text.
            * ランダムテキストに用いる文字列です。
            * @type {string}
-           * @default 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+           * @default 'abcdefghijklmnopqrstuvwxyz1234567890'
            */
-          this.sourceRandomCharacter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+          this.sourceRandomCharacter = "abcdefghijklmnopqrstuvwxyz1234567890";
           /**
-           * The string for effect space.
+           * The string for effect space.<br />
            * 空白に用いる文字列です。
-           * @type {string}
-           * @default '-'
+           * @default ''
            */
-          this.emptyCharacter = '-';
+          this.emptyCharacter = "";
           /**
-           * The milli seconds of effect time.
+           * The milli seconds of effect time.<br />
            * エフェクトの実行時間（ミリ秒）です。
-           * @type {number}
            * @default 600
            */
           this.duration = 600;
           this._isRunning = false;
-          this._originalStr = '';
+          this._originalStr = "";
           this._originalLength = 0;
           this._timeCurrent = 0;
           this._timeStart = 0;
@@ -46,78 +37,80 @@
           this._element = null;
           this._requestAnimationFrameId = 0;
           this._element = element;
-          this.setText(element.innerHTML);
+          this.setText(element.textContent ?? "");
       }
       /**
-       * Set new strings. テキストを設定します。
+       * Set new strings. <br />
+       * テキストを設定します。
        * @param text テキスト文字列です。
        */
-      ShuffleText.prototype.setText = function (text) {
+      setText(text) {
           this._originalStr = text;
           this._originalLength = text.length;
-      };
-      Object.defineProperty(ShuffleText.prototype, "isRunning", {
-          /**
-           * It is running flag. 再生中かどうかを示すブール値です。
-           * @returns {boolean}
-           */
-          get: function () {
-              return this.isRunning;
-          },
-          enumerable: true,
-          configurable: true
-      });
-      /** Play effect. 再生を開始します。 */
-      ShuffleText.prototype.start = function () {
-          var _this = this;
+      }
+      /**
+       * It is running flag. <br />
+       * 再生中かどうかを示すブール値です。
+       * @returns {boolean}
+       */
+      get isRunning() {
+          return this._isRunning;
+      }
+      /**
+       * Play effect. <br />
+       * 再生を開始します。
+       */
+      start() {
           this.stop();
           this._randomIndex = [];
-          var str = '';
-          for (var i = 0; i < this._originalLength; i++) {
-              var rate = i / this._originalLength;
-              this._randomIndex[i] = Math.random() * (1 - rate) + rate;
+          let str = "";
+          for (let i = 0; i < this._originalLength; i++) {
+              /** let rate = i / this._originalLength; **/
+              /** this._randomIndex[i] = Math.random() * (1 - rate) + rate; **/
+              this._randomIndex[i] = i / this._originalLength;
               str += this.emptyCharacter;
           }
           this._timeStart = new Date().getTime();
           this._isRunning = true;
-          this._requestAnimationFrameId = requestAnimationFrame(function () {
-              _this._onInterval();
+          this._requestAnimationFrameId = requestAnimationFrame(() => {
+              this._onInterval();
           });
-          this._element.innerHTML = str;
-      };
-      /** Stop effect. 停止します。 */
-      ShuffleText.prototype.stop = function () {
+          if (this._element) {
+              this._element.textContent = str;
+          }
+      }
+      /**
+       * Stop effect.<br />
+       * 停止します。
+       */
+      stop() {
           this._isRunning = false;
           cancelAnimationFrame(this._requestAnimationFrameId);
-      };
+      }
       /**
-       * Dispose this instance.
+       * Dispose this instance.<br />
        * メモリ解放のためインスタンスを破棄します。
        */
-      ShuffleText.prototype.dispose = function () {
+      dispose() {
           cancelAnimationFrame(this._requestAnimationFrameId);
-          this.sourceRandomCharacter = null;
-          this.emptyCharacter = null;
           this._isRunning = false;
           this.duration = 0;
-          this._originalStr = null;
+          this._originalStr = "";
           this._originalLength = 0;
           this._timeCurrent = 0;
           this._timeStart = 0;
-          this._randomIndex = null;
+          this._randomIndex = [];
           this._element = null;
           this._requestAnimationFrameId = 0;
-      };
+      }
       /**
        * インターバルハンドラーです。
-       * @private
        */
-      ShuffleText.prototype._onInterval = function () {
-          var _this = this;
+      _onInterval() {
           this._timeCurrent = new Date().getTime() - this._timeStart;
-          var percent = this._timeCurrent / this.duration;
-          var str = '';
-          for (var i = 0; i < this._originalLength; i++) {
+          const percent = this._timeCurrent / this.duration;
+          let str = "";
+          for (let i = 0; i < this._originalLength; i++) {
               if (percent >= this._randomIndex[i]) {
                   str += this._originalStr.charAt(i);
               }
@@ -125,23 +118,24 @@
                   str += this.emptyCharacter;
               }
               else {
-                  str += this.sourceRandomCharacter.charAt(Math.floor(Math.random() * (this.sourceRandomCharacter.length)));
+                  str += this.sourceRandomCharacter.charAt(Math.floor(Math.random() * this.sourceRandomCharacter.length));
               }
           }
           if (percent > 1) {
               str = this._originalStr;
               this._isRunning = false;
           }
-          this._element.innerHTML = str;
-          if (this._isRunning === true) {
-              this._requestAnimationFrameId = requestAnimationFrame(function () {
-                  _this._onInterval();
+          if (this._element) {
+              this._element.textContent = str;
+          }
+          if (this._isRunning) {
+              this._requestAnimationFrameId = requestAnimationFrame(() => {
+                  this._onInterval();
               });
           }
-      };
-      return ShuffleText;
-  }());
+      }
+  }
 
-  return ShuffleText;
+  return TextTyping;
 
 }));
